32554b4ec8efcb4ff77c355d7c6b3f99fc489b2d
KadirTopal <kadirkenantopal@gmail.com>
2022-11-18 18:05:44 -0500

    added media and state

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CardItem.css'
 import javascript from '../../assets/javascript.png'
 
-const CardItem = ({lang:{img,name}}) => {
+const CardItem = ({ lang: { img, name, options } }) => {
+
+  const [clicked, setClicked] = useState(false)
+
+  /* const clickHandler = () => {
+    setClicked(prev => !prev)
+  } */
+
+
+  let html;
+  if (clicked) {
+    html =(
+      <ul>
+        {options.map((option, index) => <li key={index.toString()}>{option}</li>)}
+      </ul>
+      )
+  } else {
+    html =(
+      <>
+        <img src={img} />
+        <h2>{name}</h2>
+      </>
+      )
+  }
 
   return (
-    <div className='CardItem'>
-      <img src={img}/>
-      <h2>{name}</h2>
-      
+    <div className='CardItem' onClick={()=> setClicked(prev => !prev)}>
+     {/*  {!clicked &&
+        <>
+          <img src={img} />
+          <h2>{name}</h2>
+        </>
+    
+      }
+
+      {clicked && <ul>{options.map((option, index) => <li key={index.toString()}>{option}</li>)}</ul>} */}
+      { html }
     </div>
+
+     
   )
 }
- 
+
 export default CardItem
